56a0b1d33cd45535abf710f6d40a7737ec350e84
andikasafri <andika.saf3@gmail.com>
2024-12-08 03:00:55 +0700

    Add background music that plays on page load

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -139,7 +139,7 @@ const Index = () => {
           muted
           className="absolute inset-0 w-full h-full object-cover" // Make the video cover the entire section
         >
-          <source src="/public/val-back.mp4" type="video/mp4" />
+          <source src="/val-back.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <div className="absolute inset-0 bg-gradient-to-b from-valorant-dark/70 to-valorant-dark" />
